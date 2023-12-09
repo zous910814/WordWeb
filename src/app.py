1ac2c296869705d068b23f79a8990cf3e620e0fe
zous910814 <zous910814@gmail.com>
2023-12-09 14:44:28 +0800

    netlify

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 # app.py
+import sys
+sys.path.append('src/')
 from flask import Flask, render_template,request
 import json
 from os import listdir
-import sys
-sys.path.append('src/')
 from utils.question import generate_question 
+from flask_frozen import Freezer
 
 app = Flask(__name__,template_folder='templates')
-
+freezer = Freezer(app)
 
 @app.route("/",methods=['GET'])
 def index():
@@ -21,6 +22,9 @@ def exam(selectFileName):
     print(data[1])
     return render_template("exam.html", selectFileName=selectFileName, questionList=data[0], answerList=data[1])
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        print("Building website...")
+        freezer.freeze()
+    else:
+        app.run(debug=True)
